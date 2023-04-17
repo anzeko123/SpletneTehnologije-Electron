450d5e3c805a97b0fde797db7b996d7234c944e1
anzeko123 <120727345+anzeko123@users.noreply.github.com>
2023-04-17 10:40:41 +0200

    Playwright testi za pregled github actions (#1)
    Co-authored-by: unknown <anze.kozole@gmail.com>

---
+++ frontend-electron/electron-tests/test.spec.js
@@ -0,0 +1,54 @@
+
+//const { _electron: electron } = require('playwright')
+//const { test, expect } = require('@playwright/test')
+/*
+test('example test', async () => {
+  const electronApp = await electron.launch({ args: ['.'] })
+  const isPackaged = await electronApp.evaluate(async ({ app }) => {
+    // This runs in Electron's main process, parameter here is always
+    // the result of the require('electron') in the main app script.
+    return app.isPackaged;
+  });
+
+  expect(isPackaged).toBe(false);
+
+  // Wait for the first BrowserWindow to open
+  // and return its Page object
+  const window = await electronApp.firstWindow()
+  window.waitForSelector('.test')
+  await window.screenshot({ path: 'intro.png' })
+
+  // close app
+  await electronApp.close()
+});
+*/
+const { _electron: electron, test, expect} = require('@playwright/test');
+
+let window;
+let electronApp;
+
+test.beforeAll(async () => {
+    electronApp = await electron.launch({ args: ['./public/main.js'] });
+    window = await electronApp.firstWindow();
+});
+
+test("Check window title.", async () => {
+  const windowName = await window.title();
+  await expect(windowName).toBe("Gis Desktop");
+});
+
+test("Check for dark mode.", async () => {
+  //await window.click('text=Dark Mode');
+  //await window.locator('Button:text("Dark mode")').click();
+  //await window.getByRole('Button').click();
+  await window.click('#toggle-dark-mode');
+  await window.screenshot({path: 'screenshots/dark-mode.png'});
+  //await expect(windowName.locator("#toggle-dark-mode")).toHaveCount(0)
+  //await expect(window.locator(".header")).toHaveCSS("color: #f1f1f1 !important")
+  //await window.emulateMedia({ colorScheme: 'light' });
+  //await window.evaluate(() => matchMedia('(prefers-color-scheme: dark)').matches);
+  //await window.evaluate(() => matchMedia('(prefers-color-scheme: light)').matches);
+  
+})
+
+
