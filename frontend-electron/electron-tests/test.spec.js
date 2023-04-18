cef1ea05b3426be34dd51413e372abe48dd08b8e
anzeko123 <120727345+anzeko123@users.noreply.github.com>
2023-04-18 19:19:31 +0200

    Update test.spec.js

--- frontend-electron/electron-tests/test.spec.js
+++ frontend-electron/electron-tests/test.spec.js
@@ -28,7 +28,7 @@ let window;
 let electronApp;
 
 test.beforeAll(async () => {
-    electronApp = await electron.launch({ args: ['./public/main.js'] });
+    electronApp = await electron.launch({ args: ['main.js'] });
     window = await electronApp.firstWindow();
 });
 
